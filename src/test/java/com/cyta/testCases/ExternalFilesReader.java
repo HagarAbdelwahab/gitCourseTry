5ca6e26182c4f70ce49a425c6ef084ffcfc0f7a1
hagar <hagar.abozaid@vodafone.com>
2020-12-11 15:13:03 +0200

    mariam stash function

--- src/test/java/com/cyta/testCases/ExternalFilesReader.java
+++ src/test/java/com/cyta/testCases/ExternalFilesReader.java
@@ -16,6 +16,10 @@ public class ExternalFilesReader {
     public void assign() throws Exception {
         ExcelReader.setExcelFile(path,"Sheet1");
     }
+    @Test
+    public void helloMariamStash() throws Exception {
+        ExcelReader.setExcelFile(path,"Sheet1");
+    }
 
 
 
